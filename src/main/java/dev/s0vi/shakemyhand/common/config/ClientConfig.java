d9b0ce1cb98bbfd4fe09ee1b3b49891c89fdb35d
Clare <orangemonkeydevelopment@gmail.com>
2022-04-18 21:27:48 -0500

    Refactor Lock out to ClientConfig

--- src/main/java/dev/s0vi/shakemyhand/common/config/ClientConfig.java
+++ src/main/java/dev/s0vi/shakemyhand/common/config/ClientConfig.java
@@ -2,9 +2,20 @@ package dev.s0vi.shakemyhand.common.config;
 
 public class ClientConfig extends Config {
     public boolean alwaysHardRestart = false;
-    public boolean instantlyTryToRejoin = true;
+    public boolean tryToRejoin = true;
     public boolean tryToResolveUnknownMods = true;
-    public boolean alwaysUserConfirmUnknownMods = false;
+
+    private String modListToLoad = "";
+    public boolean needsReload = false;
+
+    public String getModListToLoad() {
+        return modListToLoad;
+    }
+
+    public void setModListToLoad(String path) {
+        this.modListToLoad = path;
+        this.needsReload = true;
+    }
 
     public ClientConfig() {
 
